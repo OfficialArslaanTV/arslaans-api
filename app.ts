0343f935aae32c49ec227e61378381acb6786083
Arslaan <118288807+OfficialArslaanTV@users.noreply.github.com>
2024-04-04 21:05:56 +1300

    Update app.ts

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ import serverless from 'serverless-http';
 
 const app = express();
 
-app.get('/', (request, response) => response.send('💙 Netlify Functions!'));
+app.get('/', (request, response) => response.send('Arslaan\'s API!'));
 
 export default app;
 export const handler = serverless(app);
